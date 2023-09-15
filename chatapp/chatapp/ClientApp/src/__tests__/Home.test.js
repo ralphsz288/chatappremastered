32ed76433fe6acd0be0b9aca05796646466fe87f
ralphsz288 <rakph28@gmail.com>
2023-09-15 17:23:13 +0300

    added test for home.js

---
+++ chatapp/chatapp/ClientApp/src/__tests__/Home.test.js
@@ -0,0 +1,10 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import Home from "../components/Home";
+import "@testing-library/jest-dom";
+
+test("should render Home.js", () => {
+  render(<Home />);
+  const home = screen.getByTestId("home-1");
+  expect(home).toBeInTheDocument();
+  expect(home).toHaveTextContent("Hello, world!");
+});
