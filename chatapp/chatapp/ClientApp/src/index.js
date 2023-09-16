ebb07f1a754d6e53355da9d35d0d6a612c77d4df
Daniele Viola <daniele.viola02@e-uvt.ro>
2023-09-16 12:43:44 +0300

    Solved issue with test for home.js and polished react files

--- chatapp/chatapp/ClientApp/src/index.js
+++ chatapp/chatapp/ClientApp/src/index.js
@@ -1,19 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -22,5 +16,3 @@ serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
